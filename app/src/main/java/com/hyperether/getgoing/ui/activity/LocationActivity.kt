e49f98a8795ce44f49ff6bd16310262c00d07ec4
Slobodan Prijic <slobodan.prijic@gmail.com>
2019-09-17 13:19:48 +0200

    Added map into LocationActivity

--- app/src/main/java/com/hyperether/getgoing/ui/activity/LocationActivity.kt
+++ app/src/main/java/com/hyperether/getgoing/ui/activity/LocationActivity.kt
@@ -1,6 +1,114 @@
 package com.hyperether.getgoing.ui.activity
 
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.Location
+import android.location.LocationManager
+import android.os.Bundle
+import android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.databinding.DataBindingUtil
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.hyperether.getgoing.R
+import com.hyperether.getgoing.databinding.ActivityLocationBinding
 
-class LocationActivity : AppCompatActivity() {
+class LocationActivity : AppCompatActivity(), OnMapReadyCallback {
+    val REQUEST_GPS_SETTINGS = 100
+    lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val dataBinding = DataBindingUtil.setContentView<ActivityLocationBinding>(
+            this,
+            R.layout.activity_location
+        )
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.show_map_page) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        if (((ActivityCompat.checkSelfPermission(
+                this, android.Manifest.permission
+                    .ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED) && (ActivityCompat.checkSelfPermission(
+                this, android.Manifest.permission
+                    .ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED))
+        ) {
+
+            mMap = p0
+            mMap.setMyLocationEnabled(true)
+            mMap.setTrafficEnabled(true)
+            mMap.setIndoorEnabled(true)
+            mMap.setBuildingsEnabled(true)
+            mMap.getUiSettings()?.setZoomControlsEnabled(true)
+
+            val locationManager = getSystemService(
+                Context.LOCATION_SERVICE
+            ) as LocationManager
+
+            val gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+            if (!gpsEnabled) {
+                val dialog = AlertDialog.Builder(this)
+                dialog.setCancelable(false)
+                dialog.setTitle(R.string.alert_dialog_title)
+                dialog.setMessage(getString(R.string.alert_dialog_message))
+                dialog.setPositiveButton(
+                    R.string.alert_dialog_positive_button
+                ) { paramDialogInterface, paramInt ->
+                    val i = Intent(ACTION_LOCATION_SOURCE_SETTINGS)
+                    startActivityForResult(i, REQUEST_GPS_SETTINGS)
+                }
+
+                dialog.setNegativeButton(
+                    R.string.alert_dialog_negative_button,
+                    object : DialogInterface.OnClickListener {
+                        override fun onClick(paramDialogInterface: DialogInterface, paramInt: Int) {
+                            finish()
+                        }
+                    })
+
+                dialog.show()
+            }
+
+            val criteria = Criteria()
+            criteria.accuracy = Criteria.ACCURACY_FINE
+            criteria.powerRequirement = Criteria.POWER_LOW
+            val bestProvider = locationManager.getBestProvider(criteria, false)
+            val location = locationManager.getLastKnownLocation(bestProvider!!)
+            zoomOverCurrentLocation(mMap, location)
+        } else {
+            finish()
+        }
+    }
+
+
+    /**
+     * This method is used for zooming over user current location or last known location.
+     *
+     * @param googleMap google map v2
+     */
+    private fun zoomOverCurrentLocation(googleMap: GoogleMap, location: Location?) {
+        if (location != null) {
+            val latitude = location.latitude
+            val longitude = location.longitude
+
+            val center = CameraUpdateFactory.newLatLng(LatLng(latitude, longitude))
+            val zoom = CameraUpdateFactory.zoomTo(15f)
+
+            googleMap.moveCamera(center)
+            googleMap.animateCamera(zoom)
+        }
+    }
 }
